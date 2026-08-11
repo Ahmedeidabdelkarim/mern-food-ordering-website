583c430fe4d18956c9f9630eb805ff542731569a
Ahmed Eid Abdel Karim Gouda <ae1893@fayoum.edu.eg>
2026-08-11 17:44:13 +0300

    edit dns

--- src/libs/mongoConnect.js
+++ src/libs/mongoConnect.js
@@ -1,27 +1,32 @@
-import { MongoClient } from "mongodb"
+import dns from "dns";
+import { MongoClient } from "mongodb";
+
+dns.setServers(["8.8.8.8"]);
 
 if (!process.env.MONGO_URL) {
-  throw new Error('Invalid/Missing environment variable: "MONGO_URL"')
+  throw new Error('Invalid/Missing environment variable: "MONGO_URL"');
 }
 
-const uri = process.env.MONGO_URL
-const options = {}
+const uri = process.env.MONGO_URL;
+const options = {};
 
-let client
-let clientPromise
+let client;
+let clientPromise;
 
 if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  // In development mode, use a global variable so that
+  // the value is preserved across module reloads caused by HMR.
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options)
-    global._mongoClientPromise = client.connect()
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise
+
+  clientPromise = global._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options)
-  clientPromise = client.connect()
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
 }
 
-export default clientPromise
+export default clientPromise;
+
